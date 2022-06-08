d8d0f491465e8d7ce594450256fa4fe29cd88f6a
JJooaa <leitola.joa@gmail.com>
2022-06-08 20:21:11 +0300

    succesfully fetch invoices with getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import prisma from "../lib/prisma";
 import { Invoice } from "@prisma/client";
 
 export const getStaticProps = async () => {
-  const invoices = await prisma.invoice.findMany();
+  const invoices = await prisma.invoice.findMany({
+    include: {
+      items: true,
+    },
+  });
   return {
     props: {
       invoices: JSON.parse(JSON.stringify(invoices)),
@@ -13,7 +17,10 @@ export const getStaticProps = async () => {
 interface Props {
   invoices: Invoice[];
 }
+
 const Home: React.FC<Props> = (invoices) => {
+  console.log(invoices);
+
   return (
     <div className="flex flex-col items-center gap-6 justify-center h-screen"></div>
   );
