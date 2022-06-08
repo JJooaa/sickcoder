743b89acf082e1ca4d91c874c768b266530ad537
JJooaa <leitola.joa@gmail.com>
2022-06-08 10:43:48 +0300

    render posts from db with getStaticProps

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { doSomething } from "../../lib/test";
-
-type Data = {
-  result?: string;
-  error?: string;
-};
+import prisma from "../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   try {
-    const result = await doSomething(req.body);
-    res.status(200).send({ result });
+    const result = await prisma.post.findMany();
+    res.status(200).send(result);
   } catch (error) {
     res.status(500).send({ error: "failed to do action" });
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,39 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
+import prisma from "../lib/prisma";
 
-const Home: NextPage = () => {
-  return <div className="">hello</div>;
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await prisma.post.findMany();
+
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+  };
+};
+
+interface Post {
+  content: string;
+  title: string;
+  id: number;
+  createdAt: string;
+}
+
+interface Props {
+  posts: Post[];
+}
+
+const Home: React.FC<Props> = (props) => {
+  return (
+    <div className="flex flex-col items-center gap-6 justify-center h-screen">
+      {props.posts.map((post: Post) => (
+        <div key={post.id} className="bg-blue-400 rounded-md text-black p-4">
+          <h1>{post.title}</h1>
+          <p>{post.content}</p>
+          <span>{post.createdAt}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
