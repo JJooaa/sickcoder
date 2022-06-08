108bb82a3e1dc327ab5c815227cbd5d918b82f06
JJooaa <leitola.joa@gmail.com>
2022-06-08 20:33:38 +0300

    can navigate single invoice with routes with serversideprops

--- pages/invoice/[id].tsx
+++ pages/invoice/[id].tsx
@@ -1,7 +1,26 @@
-const Invoice = () => {
+import { GetServerSideProps } from "next";
+import prisma from "../../lib/prisma";
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const selectedInvoice = await prisma.invoice.findUnique({
+    where: {
+      id: String(params.id),
+    },
+    include: {
+      items: true,
+    },
+  });
+
+  return {
+    props: {
+      selectedInvoice: JSON.parse(JSON.stringify(selectedInvoice)),
+    },
+  };
+};
+
+const Invoice = (selectedInvoice) => {
+  console.log(selectedInvoice);
   return <div>Invoice</div>;
 };
 
 export default Invoice;
-
-
