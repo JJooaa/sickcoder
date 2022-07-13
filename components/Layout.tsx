d62e9a609aebe146c77d4d3be9082c807662f9a8
JJooaa <leitola.joa@gmail.com>
2022-07-13 19:38:52 +0300

    give layout ability to render multiple children

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import React from "react";
 
-const Layout = ({ children }: { children: JSX.Element }) => {
+const Layout = ({ children }: { children: JSX.Element[] }) => {
   return (
     <div className="flex flex-col">
       <Header />
