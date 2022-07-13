331a250cb10c3ca8c2a6264363c7750b5facd335
JJooaa <leitola.joa@gmail.com>
2022-07-13 19:38:02 +0300

    add progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <NextNProgress />
     </Layout>
   );
 }
