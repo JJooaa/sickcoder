47abed629879329a70f0120fe71a275b124345ef
JJooaa <leitola.joa@gmail.com>
2022-07-13 19:39:01 +0300

    initial invoice page

--- pages/invoice/[id].tsx
+++ pages/invoice/[id].tsx
@@ -4,6 +4,7 @@ import prisma from "../../lib/prisma";
 import { Invoice } from "@prisma/client";
 import { useRouter } from "next/router";
 import GoBack from "../../components/GoBack";
+import Status from "../../components/Status";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const selectedInvoice = await prisma.invoice.findUnique({
@@ -27,6 +28,7 @@ interface Props {
 }
 
 const Invoice: React.FC<Props> = ({ selectedInvoice }) => {
+  console.log(selectedInvoice);
   const {
     id,
     clientAddress,
@@ -38,6 +40,7 @@ const Invoice: React.FC<Props> = ({ selectedInvoice }) => {
     senderAddress,
     status,
     total,
+    items,
   } = selectedInvoice;
 
   const router = useRouter();
@@ -50,11 +53,76 @@ const Invoice: React.FC<Props> = ({ selectedInvoice }) => {
   };
 
   return (
-    <div className="px-6 py-8">
+    <div className="px-6 py-8 text-xs">
       <GoBack />
-      <div className="bg-white h-20">Status {status}</div>
-      <div></div>
-      <button onClick={() => deletePost(id)}>Delete</button>
+      <br />
+      <div className="bg-white flex p-5 justify-between items-center rounded-lg">
+        Status
+        <Status status={status} />
+      </div>
+      <div className="bg-white p-5 mt-4 rounded-lg text-[#7E88C3]">
+        <p className="text-black font-bold text-base uppercase">
+          #{id.slice(0, 8)}
+        </p>
+        <p>{description}</p>
+        <br />
+        <p>{senderAddress.street}</p>
+        <p>{senderAddress.city}</p>
+        <p>{senderAddress.postCode}</p>
+        <p>{senderAddress.country}</p>
+        <br />
+        <div className="flex flex-wrap gap-10">
+          <div className="flex flex-col justify-between">
+            <div>
+              <p>Invoice Date</p>
+              <p className="text-black text-base font-bold">
+                {createdAt.toString().slice(0, 10)}
+              </p>
+            </div>
+            <div>
+              <p>Payment Due</p>
+              <p className="text-black text-base font-bold">
+                {createdAt.toString().slice(0, 10)}
+              </p>
+            </div>
+          </div>
+          <div>
+            <p>Bill to</p>
+            <p className="text-black text-base font-bold">{clientName}</p>
+            <p>{clientAddress.street}</p>
+            <p>{clientAddress.city}</p>
+            <p>{clientAddress.postCode}</p>
+            <p>{clientAddress.country}</p>
+          </div>
+          <div>
+            <p>Sent To</p>
+            <p className="text-base font-bold text-black">{clientEmail}</p>
+          </div>
+        </div>
+        <br />
+        <div className="bg-[#F9FAFE] rounded-lg">
+          {items.map((item) => (
+            <div
+              key={item.name}
+              className="p-4 flex items-center justify-between"
+            >
+              <div>
+                <p className="text-black font-bold capitalize">{item.name}</p>
+                <p>
+                  {item.quantity} x € {item.price}
+                </p>
+              </div>
+              <p className="text-black font-bold">
+                $ {item.quantity * item.price}
+              </p>
+            </div>
+          ))}
+          <div className="bg-[#373B53] p-4 text-white rounded-b-lg flex justify-between items-center">
+            Grand Total: <span className="text-xl">€ {total}</span>
+          </div>
+        </div>
+      </div>
+      {/* <button onClick={() => deletePost(id)}>Delete</button> */}
     </div>
   );
 };
